4046702c31189102386633c5240c8f1de0f51b84
Peterson William <petersondev.profissional@gmail.com>
2026-01-23 15:30:51 -0300

    fix: corrigindo

--- src/modules/PaymentModule.ts
+++ src/modules/PaymentModule.ts
@@ -3,12 +3,13 @@ import { PaymentController } from 'src/controller/PaymentController';
 import Stripe from 'stripe';
 import { STRIPE_CLIENT } from 'src/config/stripe/stripe';
 import { PaymentCreateService } from 'src/service/PaymentCreateService';
+import { env } from 'process';
 
 @Module({
     providers: [
         PaymentCreateService,
         {provide: STRIPE_CLIENT,
-        useFactory: () => new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+        useFactory: () => new Stripe(env.STRIPE_SECRET_KEY ?? '', {
             apiVersion: '2025-12-15.clover',
         }),
     }],
