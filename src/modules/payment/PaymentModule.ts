d32a7570f2bb28a2e35d9c1908036efc25f12d3e
Peterson William <petersondev.profissional@gmail.com>
2026-01-23 23:40:06 -0300

    Implementando criação de intenção de pagamento

--- src/modules/payment/PaymentModule.ts
+++ src/modules/payment/PaymentModule.ts
@@ -4,10 +4,12 @@ import Stripe from 'stripe';
 import { STRIPE_CLIENT } from 'src/config/stripe/stripe';
 import { PaymentCreateService } from './service/PaymentCreateService';
 import { env } from 'process';
+import { PaymentRepository } from './repository/PaymentRepository';
 
 @Module({
     providers: [
         PaymentCreateService,
+        PaymentRepository,
         {provide: STRIPE_CLIENT,
         useFactory: () => new Stripe(env.STRIPE_SECRET_KEY ?? '', {
             apiVersion: '2025-12-15.clover',
--- src/modules/payment/dtos/CreatePaymentIntent.ts
+++ src/modules/payment/dtos/CreatePaymentIntent.ts
@@ -1,7 +1,10 @@
+import { Currency } from "../domain/enums/Currency";
+import { PaymentMethod } from "../domain/enums/PaymentMethod";
+
 export interface CreatePaymentIntent {
     reservationId: number;
     amount: number;
-    method: string;
-    currency?: string;
+    method: PaymentMethod;
+    currency?: Currency;
     customerEmail?: string;
 }
