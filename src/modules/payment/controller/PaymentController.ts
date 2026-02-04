81c55d3d1c7fe5e7f67bd5192b1e9d4134b523c5
Peterson William <petersondev.profissional@gmail.com>
2026-02-03 21:24:01 -0300

    feat: Add capture function to application controller

--- src/modules/payment/controller/PaymentController.ts
+++ src/modules/payment/controller/PaymentController.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Delete, ForbiddenException, Get, HttpCode, Inject, NotFoundException, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, ForbiddenException, Get, HttpCode, NotFoundException, Param, Post } from '@nestjs/common';
 import type { CreatePaymentIntent } from 'src/modules/payment/dtos/CreatePaymentIntent';
 import * as zod from 'zod'
 import { CreatePaymentService } from '../service/CreatePaymentService';
@@ -10,13 +10,20 @@ import { FindPaymentByIdService } from '../service/FindPaymentByIdService';
 import { PaymentNotFound } from '../domain/errors/PaymentNotFound';
 import { RefundPaymentService } from '../service/RefundPaymentService';
 import { PaymentCannotBeRefunded } from '../domain/errors/PaymentCannotBeRefunded';
+import { AmountCaptureService } from '../service/AmountCaptureService';
+import type { PaymentCaptureDTO } from '../dtos/PaymentCaptureDTO';
+import { PaymentAutomaticCapture } from '../domain/errors/PaymentAutomaticCapture';
+import { PaymentCannotBeCaptured } from '../domain/errors/PaymentCannotBeCaptured';
+import { ValueAbovePermitted } from '../domain/errors/ValueAbovePermitted';
+import { StripeError } from '../domain/errors/StripeError';
 
 @Controller('payment')
 export class PaymentController {
     constructor(private readonly createService: CreatePaymentService,
                 private readonly findPaymentByReservationService : FindPaymentByReservationService,
                 private readonly findPaymentByIdService : FindPaymentByIdService,
-                private readonly refundPaymentService : RefundPaymentService) {}
+                private readonly refundPaymentService : RefundPaymentService,
+                private readonly capturePaymentService : AmountCaptureService) {}
 
     @Post()
     @HttpCode(201)
@@ -25,11 +32,9 @@ export class PaymentController {
             const payment = await this.createService.execute(data)
             return { payment }
         } catch (error) {
-            if (error instanceof zod.ZodError) {
-                throw new BadRequestException({message: "Dados inválidos"})
-            } else if (error instanceof AmountZero) {
-                throw new BadRequestException({message: error.message})
-            }
+            if (error instanceof zod.ZodError) throw new BadRequestException({message: "Dados inválidos"})
+            if (error instanceof AmountZero) throw new BadRequestException({message: error.message})
+            throw Error("Unhandled error!")
         }
     }
 
@@ -42,6 +47,7 @@ export class PaymentController {
         } catch (error) {
             if (error instanceof InvalidId) throw new BadRequestException(error.message)
             if (error instanceof PaymentNotFound) throw new NotFoundException(error.message)
+            throw Error("Unhandled error!")
         }
     }
 
@@ -55,6 +61,7 @@ export class PaymentController {
         } catch (error) {
             if (error instanceof ReservationPaymentNotFound) throw new NotFoundException(error.message)
             if (error instanceof InvalidId) throw new BadRequestException(error.message)
+            throw Error("Unhandled error!")
         }
     }
 
@@ -68,6 +75,27 @@ export class PaymentController {
             if (error instanceof InvalidId) throw new BadRequestException(error.message)
             if (error instanceof PaymentNotFound) throw new NotFoundException(error.message)
             if (error instanceof PaymentCannotBeRefunded) throw new ForbiddenException(error.message)
+            throw Error("Unhandled error!")
         }
     }
+
+    @Post('/capture')
+    @HttpCode(200)
+    async capturePayment(@Body() data : PaymentCaptureDTO) {
+        try {
+            const caputure = await this.capturePaymentService.execute(data)
+            return { caputure }
+        } catch (error) {
+            if (error instanceof zod.ZodError) throw new BadRequestException("Invalid data!")
+            if (error instanceof AmountZero) throw new BadRequestException(error.message)
+            if (error instanceof ValueAbovePermitted) throw new BadRequestException(error.message)
+            if (error instanceof BadRequestException) throw new BadRequestException(error.message)
+            if (error instanceof PaymentNotFound) throw new NotFoundException(error.message)
+            if (error instanceof PaymentAutomaticCapture) throw new ForbiddenException(error.message)
+            if (error instanceof PaymentCannotBeCaptured) throw new ForbiddenException(error.message)
+            if (error instanceof StripeError) throw new Error(error.message)
+            throw Error("Unhandled error!")
+        }
+    }
+
 }
